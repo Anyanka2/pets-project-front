0a8eab0b5b276c71ef99945e358006a7c49c66e2
lex <savchenko.lex@gmail.com>
2023-11-17 16:41:45 +0200

    router first step

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from '../src/components/Header/Header';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-    </div>
+    <Routes>
+      <Route path='/' element={<Header/>}>
+        
+        
+      </Route> 
+    </Routes>
   );
 };
 
