acef6d5ffbbd28fc4a4ca3f0965d5c2f5d6299b0
gasanaliguliev <aligulievgasan001@gmail.com>
2023-11-18 16:51:06 -0500

    fixed button styles

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,19 @@
 import { NavigationContainer } from '../Navigation/Navigation.styled';
 import { NavLink } from 'react-router-dom'; 
 
+
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <NavLink to="news">News</NavLink>
-      <NavLink to="/">Find Pet</NavLink>
-      <NavLink to="friends">Our Friends</NavLink>
+      <NavLink to="news">
+        News
+      </NavLink>
+      <NavLink to="/">
+        Find Pet
+      </NavLink>
+      <NavLink to="friends">
+        Our Friends
+      </NavLink>
     </NavigationContainer>
   );
 };
