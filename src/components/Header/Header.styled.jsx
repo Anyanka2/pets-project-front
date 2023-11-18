5001f3a2bc1c23b12492edf68c0cfdecd2cc3715
gasanaliguliev <aligulievgasan001@gmail.com>
2023-11-17 19:12:33 -0500

    fix header

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -3,28 +3,41 @@ import styled from 'styled-components';
 // Стилизация Header
 export const HeaderContainer = styled.div`
   display: flex;
-  justify-content: space-between;
   align-items: center;
-  padding: 20px;
-  background-color: #333;
-  color: #fff;
+  padding: 30px 16px;
+  color: #111;
 `;
 
 export const Logo = styled.div`
   font-size: 24px;
   font-weight: bold;
+  margin-right: 159px;
 `;
+
+export const Image = styled.img`
+  width: 162px;
+  height: 28px;
+`;
+
 
 export const ButtonsContainer = styled.div`
   display: flex;
   gap: 10px;
+
+ 
 `;
 
 export const Button = styled.button`
-  padding: 8px, 20px, 8px, 20px
-  background-color: #FFC107;
-  color: #333;
-  border: 2px;
+  padding: 8px 20px 8px 20px;
+  border: 2px solid #FFC107;
   cursor: pointer;
+  color: #FFC107;
   width: 165px;
+  border-radius: 40px;
+  background-color: #fff;
+
+  &:hover {
+    background-color: #FFC107;
+    color: #fff;
+  }
 `;
