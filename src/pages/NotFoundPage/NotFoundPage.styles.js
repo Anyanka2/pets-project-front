8ed0e3cacb95ddb47bd6ec96583273624f7e968c
Bohdan99I <bohdan.voiyov@gmail.com>
2023-11-19 16:36:45 +0200

    done

--- src/pages/NotFoundPage/NotFoundPage.styles.js
+++ src/pages/NotFoundPage/NotFoundPage.styles.js
@@ -13,4 +13,38 @@ export const Title = styled.h2`
 
 export const Image = styled.img`
   max-width: 100%;
+  border-radius: 4px;
+`;
+
+export const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  padding: 8px 28px;
+  margin: 24px 0 40px 0;
+  width: 248px;
+  height: 40px;
+  background: #54adff;  
+  border: 1px solid #54adff;
+  border-radius: 40px;
+  color: #fef9f9;
+  transform: scale(1);
+  transition: transform 0.3s;
+  font-family: 'Manrope';
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 22px; 
+  cursor: pointer;
+  gap: 12px;
+  position: relative;
+
+  &:hover,
+  &:focus {
+    transform: scale(1.05);
+    transition: transform 0.3s;
+  }
+  &:hover:before {
+    left: 100%;
+  } 
 `;
