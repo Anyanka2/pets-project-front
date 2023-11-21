8a822c82a6c9ef220085a14e80b3b80671ef312f
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-11-21 11:22:20 +0000

    Deploying to gh-pages from @ Anyanka2/pets-project-front@88818908d753f8f26d9d4eb45b92196d272c00e4 🚀

--- static/js/982.5fadfdd2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpets_project_front=self.webpackChunkpets_project_front||[]).push([[982],{982:(e,s,t)=>{t.r(s),t.d(s,{default:()=>c});var r=t(184);const c=()=>(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("div",{children:"This is the User Page!"})})}}]);
-//# sourceMappingURL=982.5fadfdd2.chunk.js.map
