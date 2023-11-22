0ded76185dd42a4b47d226213c597d8eb5e38699
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-11-22 16:22:49 +0100

    add searchbar, some fixies

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -1,9 +1,11 @@
 import { TitlePage } from "../../shared/components/TitlePage.styled";
+import { SearchBar } from "../../shared/components/SearchBar/SearchBar";
 
 const NoticesPage = () => {
 
     return (
         <>
+            <SearchBar />
         <TitlePage>This is the Notices Page!</TitlePage>
         </>
     )
