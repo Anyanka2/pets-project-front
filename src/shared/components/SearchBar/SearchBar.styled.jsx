a2bf97d8f7c3f98e84521ee880169aa9fa48b62b
Sergey Babenko <serg_babenko@i.ua>
2023-11-23 02:04:09 +0200

    Search-styles

--- src/shared/components/SearchBar/SearchBar.styled.jsx
+++ src/shared/components/SearchBar/SearchBar.styled.jsx
@@ -1,5 +1,45 @@
 import styled from "styled-components";
 
-export const Search = styled.input`
+export const SearchForm = styled.form`
+  padding-left: 20px;
+  position: relative;
   max-width: 608px;
+  height: 44px;
+  @media screen and (max-width: 479px) {
+    max-width: 280px;
+  }
+`;
+
+export const SearchInput = styled.input`
+  font: Inter;
+  font-weight: 400;
+  font-size: 20px;
+  color: #888888;
+  padding-left: 20px;
+  height: 44px;
+  width: 100%;
+  background-color: #ffffff;
+  border-radius: 20px;
+  border: 0;
+  box-shadow: 3px 8px 14px rgba(136, 198, 253, 0.19);
+  @media screen and (max-width: 479px) {
+    border-radius: 24px;
+    font-size: 14px;
+  }
+`;
+
+export const SearchButton = styled.button`
+  position: absolute;
+  height: 24px;
+  width: 24px;
+  top: 10px;
+  right: 20px;
+  padding: 0;
+
+  background-color: #f7b75d;
+  color: #fff;
+  font-size: 16px;
+  @media screen and (max-width: 479px) {
+    right: 12px;
+  }
 `;
