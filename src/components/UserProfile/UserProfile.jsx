87752f1123606ebb468097ef86d897d2478ebb3f
lex <savchenko.lex@gmail.com>
2023-11-24 14:05:37 +0200

    create separate form element

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,40 +1,19 @@
 import { useState } from "react";
-import { Formik } from "formik";
-import { object, string, number, date, InferType } from "yup";
 
 import UserPhotoDefault from "../../assets/icons/user_photo_default.svg";
 import {
   UserProfileContainer,
   UserPhotoBox,
-  LogoutBtn,
   UserImg,
-  Label,
-  SubmitBtn,
   EditImg,
   UserProfileSection,
-  StyledForm,
-  StyledField,
 } from "./UserProfile.styled";
+import UserProfileForm from "./UserProfileForm/UserProfileForm";
 
 export default function UserProfile() {
   const [editable, setEditable] = useState(false);
-  const [anketa, setAnketa] = useState({
-    name: "",
-    email: "",
-    birthday: "",
-    phone: "",
-    city: "",
-  });
 
-  const yupSchema = object().shape({
-    name: string().required("Name is required"),
-    email: string()
-      .email("Invalid email address")
-      .required("Email is required"),
-    birthday: string(),
-    phone: string(),
-    city: string(),
-  });
+  
 
   const editHandler = (e) => {
     if (e.target.nodeName === "IMG") {
@@ -43,9 +22,7 @@ export default function UserProfile() {
     setEditable((pervState) => !pervState);
   };
 
-  const handleLogout = async (e) => {
-    console.log("logout");
-  };
+  
 
   return (
     <>
@@ -56,50 +33,7 @@ export default function UserProfile() {
             <UserImg src={UserPhotoDefault} alt="User profile" />
             {editable ? <EditImg>Edit photo</EditImg> : ""}
           </UserPhotoBox>
-          <Formik
-            initialValues={anketa}
-            validationSchema={yupSchema}
-            onSubmit={(values, actions) => {
-              //  setTimeout(() => {
-              //    alert(JSON.stringify(values, null, 2));
-              //    actions.setSubmitting(false);
-              //  }, 1000);
-              console.log("this is submit");
-              actions.setSubmitting(false)
-            }}
-          >
-            {(props) => (
-              <StyledForm onSubmit={props.onSubmit}>
-                <Label>
-                  Name:
-                  <StyledField disabled={!editable} type="text" name="name" />
-                </Label>
-                <Label>
-                  Email:
-                  <StyledField disabled={!editable} type="text" name="email" />
-                </Label>
-                <Label>
-                  Birthday:
-                  <StyledField disabled={!editable} type="text" name="birthday" />
-                </Label>
-                <Label>
-                  Phone:
-                  <StyledField disabled={!editable} type="text" name="phone" />
-                </Label>
-                <Label>
-                  City:
-                  <StyledField disabled={!editable} type="text" name="city" />
-                </Label>
-                {editable ? (
-                  <SubmitBtn type="submit">Submit</SubmitBtn>
-                ) : (
-                  <LogoutBtn type="button" onClick={handleLogout}>
-                    Log Out
-                  </LogoutBtn>
-                )}
-              </StyledForm>
-            )}
-          </Formik>
+          <UserProfileForm editable={editable} />
         </UserProfileContainer>
       </UserProfileSection>
     </>
---
+++ src/components/UserProfile/UserProfileForm/UserProfileForm.jsx
@@ -0,0 +1,128 @@
+import {
+    LogoutBtn,
+  StyledErrorMessage,
+  StyledField,
+  StyledForm,
+  StyledInputWrapper,
+  StyledLabel,
+  StyledSubmitBtn,
+} from "./UserProfileForm.styles.jsx";
+import { ErrorMessage, Formik } from "formik";
+import { useState } from "react";
+import * as Yup from "yup";
+
+export default function UserProfileForm(props) {
+  const [userData, setUserData] = useState({
+                name: "Ann",
+                email: "aaaa@gmail.com",
+                birthday: "00.00.0000",
+                phone: "+380972548888",
+                city: "Kyiv"
+        });
+
+  const handleLogout = async (e) => {
+    console.log("logout");
+  };
+
+  return (
+    <Formik
+      initialValues={userData}
+      validationSchema={Yup.object({
+        name: Yup.string().required("Name is required"),
+        email: Yup.string()
+          .email("Invalid email address")
+          .required("Email is required"),
+        birthday: Yup.string(),
+        phone: Yup.string(),
+        city: Yup.string(),
+      })}
+      onSubmit={(values) => {
+        console.log(values);
+        setUserData({
+            name: "Ann",
+            email: "aaaa@gmail.com",
+            birthday: "00.00.0000",
+            phone: "+380972548888",
+            city: "Kyiv"
+          });
+      }}
+    >
+      <StyledForm>
+        <StyledLabel htmlFor="name">
+          <div>Name:</div>
+          <StyledInputWrapper>
+            <StyledField
+              id="name"
+              type="text"
+              name="name"
+              disabled={!props.editable}
+            />
+            <StyledErrorMessage>
+              <ErrorMessage name="name" />
+            </StyledErrorMessage>
+          </StyledInputWrapper>
+        </StyledLabel>
+        <StyledLabel htmlFor="email">
+          <div>Email:</div>
+          <StyledInputWrapper>
+            <StyledField
+              id="email"
+              type="text"
+              name="email"
+              disabled={!props.editable}
+            />
+            <StyledErrorMessage>
+              <ErrorMessage name="email" />
+            </StyledErrorMessage>
+          </StyledInputWrapper>
+        </StyledLabel>
+        <StyledLabel htmlFor="birthday">
+          <div>Birthday:</div>
+          <StyledInputWrapper>
+            <StyledField
+              id="birthday"
+              type="text"
+              name="birthday"
+              disabled={!props.editable}
+            />
+            <StyledErrorMessage>
+              <ErrorMessage name="birthday" />
+            </StyledErrorMessage>
+          </StyledInputWrapper>
+        </StyledLabel>
+        <StyledLabel htmlFor="phone">
+          <div>Phone:</div>
+          <StyledInputWrapper>
+            <StyledField
+              id="phone"
+              type="text"
+              name="phone"
+              disabled={!props.editable}
+            />
+            <StyledErrorMessage>
+              <ErrorMessage name="phone" />
+            </StyledErrorMessage>
+          </StyledInputWrapper>
+        </StyledLabel>
+        <StyledLabel htmlFor="city">
+          <div>City:</div>
+          <StyledInputWrapper>
+            <StyledField
+              id="city"
+              type="text"
+              name="city"
+              disabled={!props.editable}
+            />
+            <StyledErrorMessage>
+              <ErrorMessage name="city" />
+            </StyledErrorMessage>
+          </StyledInputWrapper>
+        </StyledLabel>
+        { props.editable ? 
+            <StyledSubmitBtn type="submit">Save</StyledSubmitBtn> :
+            <LogoutBtn type="button" onClick={handleLogout}>Log Out</LogoutBtn>
+        }
+      </StyledForm>
+    </Formik>
+  );
+}
