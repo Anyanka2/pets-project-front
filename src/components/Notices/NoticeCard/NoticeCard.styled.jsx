be752b47b9f8ec705d1f4c4428d2d7475bb693ef
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-11-24 14:19:48 +0100

    fixed button style

--- src/components/Notices/NoticeCard/NoticeCard.styled.jsx
+++ src/components/Notices/NoticeCard/NoticeCard.styled.jsx
@@ -170,16 +170,19 @@ export const LearnMoreBtn = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+  font-size: inherit;
+  color: ${theme.colors.blueLink};
   width: 248px;
   height: 38px;
   padding: 8px 28px;
   border-radius: 40px;
   border: 2px solid ${theme.colors.blueLink};
-  background-color: transparent;
-  
+  background: transparent;
+  margin: 0 auto;
 
-  &:hover {
-    transform: scale(1.02);
-    transition: transform ${theme.transitions.transitionRegular};
+  &:hover,
+  &:focus {
+    background: ${theme.colors.blueGradient};
+    color: ${theme.colors.white};
   }
 `;
