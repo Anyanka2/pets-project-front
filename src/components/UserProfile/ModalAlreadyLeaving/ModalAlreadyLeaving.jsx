64a8055d9d5ed443d30fff73eeca90ba85b77385
Mr-Desperado-19 <mr.arturhlebov@gmail.com>
2023-11-26 20:35:46 +0100

    fixed icon problem

--- src/components/UserProfile/ModalAlreadyLeaving/ModalAlreadyLeaving.jsx
+++ src/components/UserProfile/ModalAlreadyLeaving/ModalAlreadyLeaving.jsx
@@ -1,6 +1,4 @@
 import {
-  CloseButton,
-  CloseIconSVG,
   Modal,
   TitleText,
   CancelButton,
@@ -9,22 +7,21 @@ import {
   LogoutIconSVG,
 } from "./ModalAlreadyLeaving.styled";
 
-import CloseIcon from "../../../assets/icons/cross-small-blue.svg";
 // import LogoutIcon from "../../../assets/icons/logout.svg";
+import { ReactComponent as LogoutIcon  } from "../../../assets/icons/logout.svg";
 
 export const ModalAlreaadyLeaving = () => {
     return (
         <>
-            <CloseButton>
-                <CloseIconSVG src={CloseIcon} />
-            </CloseButton>
-
             <Modal>
                 <TitleText>Already leaving?</TitleText>
                 <CancelButton>Cancel</CancelButton>
                 <LogoutButton>
                     <LogoutButtonText>Yes</LogoutButtonText>
-                    <LogoutIconSVG />
+                    <LogoutIconSVG>
+                        <LogoutIcon />
+                    </LogoutIconSVG>
+                        
                 </LogoutButton>
             </Modal>
         </>
