ba0c0649d624307aa9faa29833d4358fb9c77a3b
Mr-Desperado-19 <mr.arturhlebov@gmail.com>
2023-11-26 18:35:43 +0100

    Update Modal Already Leaving

--- src/components/UserProfile/ModalAlreadyLeaving/ModalAlreadyLeaving.jsx
+++ src/components/UserProfile/ModalAlreadyLeaving/ModalAlreadyLeaving.jsx
@@ -1,7 +1,33 @@
+import React from 'react';
+import {
+  CloseButton,
+  CloseIcon,
+  Modal,
+  TitleText,
+  CancelButton,
+  LogoutButton,
+  LogoutButtonText,
+  LogoutIcon,
+} from "./ModalAlreadyLeaving.styled";
+
+import { ReactComponent as CloseIcon } from "../../../assets/icons/cross-small-blue.svg";
+import { ReactComponent as LogoutIcon } from "../../../assets/icons/logout.svg";
+
 export const ModalAlreaadyLeaving = () => {
     return (
         <>
-        <p>AlreaadyLeaving</p>
+            <CloseButton>
+                <CloseIcon />
+            </CloseButton>
+
+            <Modal>
+                <TitleText>Already leaving?</TitleText>
+                <CancelButton>Cancel</CancelButton>
+                <LogoutButton>
+                    <LogoutButtonText>Yes</LogoutButtonText>
+                    <LogoutIcon />
+                </LogoutButton>
+            </Modal>
         </>
     )
 }
---
+++ src/components/UserProfile/ModalAlreadyLeaving/ModalAlreadyLeaving.styled.jsx
@@ -0,0 +1,25 @@
+import { styled } from "styled-components";
+
+export const CloseButton = styled.button`
+`;
+
+export const CloseIcon = styled.svg`
+`;
+
+export const Modal = styled.div`
+`;
+
+export const TitleText = styled.h2`
+`;
+
+export const CancelButton = styled.button`
+`;
+
+export const LogoutButton = styled.button`
+`;
+
+export const LogoutButtonText = styled.p`
+`;
+
+export const LogoutIcon = styled.svg`
+`;
