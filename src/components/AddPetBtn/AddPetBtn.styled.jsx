ae4c9f7ab7ca9972492d045c1f23e6d04be233ac
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-11-26 14:39:14 +0100

    edit addpet button

--- src/components/AddPetBtn/AddPetBtn.styled.jsx
+++ src/components/AddPetBtn/AddPetBtn.styled.jsx
@@ -3,23 +3,28 @@ import { theme } from "../../shared/styles/theme";
 import { Link } from "react-router-dom";
 
 export const AddPetLink = styled(Link)`
-  display: none;
+  position: fixed;
+  z-index: 99;
+  top: 50%;
+  translate: 0 -50%;
+  right: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 4px;
   width: 80px;
-  text-align: center;
-  font-family: ${theme.fonts.manrope.regular};
-  font-size: 12px;
-  font-weight: ${theme.fontWeights.regular};
-  color: ${theme.colors.white};
-  background-color: ${theme.colors.blueLink};
-  border-radius: 40px;
+  height: 80px;
+  border-radius: 50px;
   border: none;
-  padding: 8px 20px;
-  //z-index: 999;
-  //opacity: 1;
-
+  background: ${theme.colors.blueLink};
+  box-shadow: ${theme.shadows.shadowDefault};
+  font-family: ${theme.fonts.manrope};
+  font-weight: ${theme.fontWeights.semiBold};
+  font-size: 12px;
+  line-height: 1.33;
+  color: ${theme.colors.white};
   cursor: pointer;
-  outline: none;
-  
 
   &:focus,
   &:hover {
@@ -28,19 +33,22 @@ export const AddPetLink = styled(Link)`
   }
 
   @media screen and (min-width: 768px) {
-    display: flex;
-    width: 129px;
-    align-items: center;
-    justify-content: center;
+    position: static;
+    margin-top: 60px;
+    margin-left: auto;
     gap: 8px;
-    padding: 8px 20px;
+    flex-direction: row;
+    width: 129px;
+    background: ${theme.colors.blueLink};
     border-radius: 40px;
+    height: 40px;
+    font-weight: ${theme.fontWeights.bold};
     font-size: 16px;
-    font-weight: ${theme.fontWeights.regular};
-    //letter-spacing: 0.64px;
+    line-height: 1.375;
   }
-
-  svg {
-    stroke: ${theme.colors.white};
-  }
+`;
+export const PlusIcon = styled.svg`
+  width: 24px;
+  height: 24px;
+  stroke: ${theme.colors.white};
 `;
