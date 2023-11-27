4e4f8ae31fc3695f41d07bc53066cb03db6bb0cd
Mr-Desperado-19 <mr.arturhlebov@gmail.com>
2023-11-27 23:30:07 +0100

    Update modal styles

--- src/components/UserProfile/ModalAlreadyLeaving/ModalAlreadyLeaving.styled.jsx
+++ src/components/UserProfile/ModalAlreadyLeaving/ModalAlreadyLeaving.styled.jsx
@@ -1,21 +1,17 @@
 import { styled } from "styled-components";
 
 export const Modal = styled.div`
-width: 100%;
+width: 232px;
 text-align: center;
 align-items: center;
 justify-content: center;
-padding-top: 77px;
-padding-bottom: 60px;
-padding-left: 12px;
-padding-right: 12px;
+padding-top: 53px;
+padding-bottom: 36px;
 
   @media screen and (min-width: 768px) {
-    width: 608px;
-    padding-top: 108px;
-    padding-bottom: 109px;
-    padding-left: 24px;
-    padding-right: 24px;
+    width: 536px;
+    padding-top: 84px;
+    padding-bottom: 85px;
   }
 `;
 
@@ -27,7 +23,7 @@ export const TitleText = styled.h2`
   color: #111111;
   margin-bottom: 44px;
 
-    @media screen and (min-width: 768px) {
+  @media screen and (min-width: 768px) {
     font-size: 36px;
     margin-bottom: 48px;
   }
@@ -36,24 +32,58 @@ export const TitleText = styled.h2`
 export const ButtonContainer = styled.div`
 display: flex;
 flex-direction: column;
+justify-content: center;
 gap: 8px;
 
-    @media screen and (min-width: 768px) {
+  @media screen and (min-width: 768px) {
     flex-direction: row;
     gap: 17px;
   }
 `;
 
 export const CancelButton = styled.button`
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 1.36;
+  letter-spacing: 0.04em;
+  color: #54ADFF;
+  border: 2px solid #54ADFF;
+  border-radius: 40px;
+
+  width: 256px;
+  padding-top: 9px;
+  padding-bottom: 9px;
+
+  @media screen and (min-width: 768px) {
+    width: 129px;
+  }
 `;
 
 export const LogoutButton = styled.button`
+  display: flex;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  width: 256px;
+  background-color: #54ADFF;
+  border-radius: 40px;
+
+  @media screen and (min-width: 768px) {
+    width: 129px;
+  }
 `;
 
 export const LogoutButtonText = styled.p`
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 1.36;
+  letter-spacing: 0.04em;
+  color: #FEF9F9;
+
+  padding-top: 9px;
+  padding-bottom: 9px;
 `;
 
 export const LogoutIconSVG = styled.div`
-display: flex;
-align-items: center;
 `;
