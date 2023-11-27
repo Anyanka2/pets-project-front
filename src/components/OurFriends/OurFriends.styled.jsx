71b2c5a5db62f25b44e89f9ccb6bb1bacf6109c2
Bohdan99I <bohdan.voiyov@gmail.com>
2023-11-27 22:09:20 +0200

    Fix-infa_our_friends

--- src/components/OurFriends/OurFriends.styled.jsx
+++ src/components/OurFriends/OurFriends.styled.jsx
@@ -1,59 +1,101 @@
 import styled from "@emotion/styled";
 import { theme } from "../../shared/styles/theme.jsx";
 
-export const Title = styled.h2`
-  text-align: center;
-  font-size: 24px;
+export const StyledList = styled.ul`
+  margin-top: 24px;
+  margin-bottom: 24px;
+
+  @media screen and (min-width: 748px) {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: start;
+    row-gap: 20px;
+  }
 `;
 
-export const List = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: start;
-`;
-
-export const Item = styled.li`
+export const StyledItem = styled.li`
   text-align: center;
   border-radius: 40px;
   padding: 16px;
   box-shadow: ${theme.shadows.shadowDefault};
   background-color: ${theme.colors.white};
-  width: 395px;
-  height: 287px;
+  width: 280px;
   margin-left: auto;
   margin-right: auto;
+
+  @media screen and (max-width: 767px) {
+    :not(:first-of-type) {
+      margin-top: 20px;
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 336px;
+    height: 275px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 395px;
+    height: 287px;
+  }
 `;
 
-export const Name = styled.a`
+export const StyledName = styled.a`
   font-size: 24px;
-  font-weight: 700;
+  font-weight: ${theme.fontWeights.bold};
   color: ${theme.colors.blueLink};
   text-decoration: none;
 `;
 
-export const ImgThumb = styled.div`
-  width: 146px;
+export const StyledImgThumb = styled.div`
+  @media screen and (max-width: 747px) {
+    width: 90px;
+  }
+
+  @media screen and (min-width: 748px) and (max-width: 1279px) {
+    width: 140px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 146px;
+  }
 `;
 
-export const Image = styled.img`
+export const StyledImage = styled.img`
   max-width: 100%;
 `;
 
-export const Contacts = styled.p`
+export const StyledContacts = styled.p`
   font-size: 12px;
-  font-weight: 500;
+  font-weight: ${theme.fontWeights.medium};
   color: ${theme.colors.black};
   margin-top: 12px;
+
+  @media screen and (min-width: 748px) and (max-width: 1279px) {
+    font-size: 14px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 16px;
+  }
 `;
 
-export const ContactsLink = styled.a`
+export const StyledContactsLink = styled.a`
   text-decoration: none;
   font-size: 12px;
-  font-weight: 500;
+  font-weight: ${theme.fontWeights.medium};
   color: ${theme.colors.black};
   transition: color: ${theme.transitions.transitionRegular};
 
   :hover {
-    color: color: ${theme.colors.blueLink};
+    color: ${theme.colors.blueLink};
+  }
+
+  @media screen and (min-width: 748px) and (max-width: 1279px) {
+    font-size: 14px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 16px;
   }
 `;
